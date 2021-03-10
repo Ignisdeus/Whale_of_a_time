941fac660fdcd6f3d7c6b3f55d9546f483a4e8af
Michael Carstiars <michael.j.carstairs@gmail.com>
2021-03-10 19:43:25 +0000

    End of session Two

--- Whale_ofA_Time/Assets/Scripts/Whale_Movement.cs
+++ Whale_ofA_Time/Assets/Scripts/Whale_Movement.cs
@@ -11,6 +11,9 @@ public class Whale_Movement : MonoBehaviour
 
     public float speed = 5f; // Whale movement speed 
     public float minX, maxX; // min and max movement constraint 
+    public float jumpForce = 250f;// how hard / high I jump 
+      
+    public KeyCode jump; // pick a key  
     void Update()
     {
         // get input from Horizontal movement 
@@ -24,12 +27,25 @@ public class Whale_Movement : MonoBehaviour
         if(transform.position.x > maxX){
             transform.position = new Vector2(maxX, transform.position.y); 
         }
+        // if I hit the jump key and I am grounded jump.
+        if(Input.GetKeyDown(jump) && grounded == true){
+            grounded = false; 
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other){
-        	// if I collide with the ball display debug. 
+        // if I collide with the ball display debug. 
         if(other.gameObject.tag =="Ball"){
             Debug.Log("Hit the Ball");
+        }
+    }
+    // bool for checking if this game object is grounded 
+    public bool grounded = false; 
+    void OnTriggerStay2D(Collider2D other){
+        // if im on the ground im grounded
+        if(other.gameObject.tag =="Ground"){
+            grounded = true; 
         }
     }
 }
